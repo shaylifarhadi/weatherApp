605a68b192c57fed2a71095daf06ffffc85629db
pcfix <farhadishayli1401@gmail.com>
2022-04-07 09:31:38 +0430

    weatherAppWithMap

--- app/src/main/java/com/example/mymap/ui/DailyItem.kt
+++ app/src/main/java/com/example/mymap/ui/DailyItem.kt
@@ -8,31 +8,38 @@ import com.xwray.groupie.databinding.BindableItem
 import java.text.SimpleDateFormat
 import java.util.*
 
-class DailyItem(val daily: Daily) : BindableItem<ItemDailyBinding>() {
+class DailyItem(val daily : Daily) : BindableItem<ItemDailyBinding>() {
+
+    companion object {
+
+        var celTemp : String = ""
+    }
+
     @SuppressLint("SetTextI18n")
-    override fun bind(dataBinding: ItemDailyBinding, position: Int) {
+    override fun bind(dataBinding : ItemDailyBinding, position : Int) {
         dataBinding.daily = daily
 
         val kelvinTemp = daily.temp?.day ?: 0
 
         dataBinding.tvDay.text = formatDate((daily.dt?.toLong()?.times(1000)) ?: 0)
 
-        val celTemp = toCelsius((kelvinTemp) as Double).toString()
+        celTemp = toCelsius((kelvinTemp) as Double).toString()
 
-        dataBinding.tvTemp.text = celTemp.substring(0,celTemp.indexOf(".")-1 )
+        dataBinding.tvTemp.text = celTemp.substring(0, celTemp.indexOf(".") - 1)
     }
+
     override fun getLayout() = R.layout.item_daily
 
-    private fun toCelsius(kelvinTemp:Double):Double =
+    private fun toCelsius(kelvinTemp : Double) : Double =
         kelvinTemp - 273.15F
 
     @SuppressLint("SimpleDateFormat")
-    private fun formatDate(date: Long?): String {
+    private fun formatDate(date : Long?) : String {
         return try {
             val dateFormat = SimpleDateFormat("yyyy/MM/dd")
             val date = Date(date ?: 0)
             dateFormat.format(date)
-        } catch (e: Exception) {
+        } catch (e : Exception) {
             e.toString()
         }
     }
--- app/src/main/java/com/example/mymap/ui/MainFragment.kt
+++ app/src/main/java/com/example/mymap/ui/MainFragment.kt
@@ -1,31 +1,37 @@
 package com.example.mymap.ui
 
-import android.annotation.SuppressLint
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import com.example.mymap.R
 import com.example.mymap.databinding.MainFragmentBinding
 import com.example.mymap.network.datamodel.Daily
 import com.example.mymap.network.errorhandling.Status
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.xwray.groupie.GroupieAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
-import java.text.SimpleDateFormat
-import java.util.*
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
 
     companion object {
         const val TAG = "MainFragment"
+        const val REQUEST_LOCATION_PERMISSION = 1000
     }
 
     private val viewModel by viewModels<MainViewModel>()
@@ -51,6 +57,7 @@ class MainFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync { map ->
             viewModel.map = map
+            enableMyLocation()
 
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(iranLatLng, 4f))
 
@@ -71,7 +78,11 @@ class MainFragment : Fragment() {
                 Timber.i(it.message.toString())
             } else {
                 val getData = it.data
-                viewModel.temp = getData?.current?.temp.toString()
+               viewModel.temp = getData?.current?.temp.toString().substring(0..1)
+
+              /*  val data = getData?.current?.temp.toString()
+                data.substring(0,data.indexOf(".")-1)
+                viewModel.temp = data*/
 
                 getData?.daily?.let {
                     createRecyclerView(it)
@@ -102,6 +113,38 @@ class MainFragment : Fragment() {
                 viewModel.temp
             )
         )?.showInfoWindow()
+    }
+
+    private fun enableMyLocation(){
+        if (isPermissionGranted()){
+            viewModel.map.isMyLocationEnabled = true
+        }else{
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION)
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode : Int,
+        permissions : Array<out String>,
+        grantResults : IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions,
+            grantResults)
+        if (requestCode == REQUEST_LOCATION_PERMISSION){
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)){
+                enableMyLocation()
+            }
+        }
+    }
+
+    private fun isPermissionGranted():Boolean{
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
     }
 }
 
